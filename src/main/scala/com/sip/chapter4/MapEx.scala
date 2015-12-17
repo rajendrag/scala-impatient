bc2d9b2b4ca52238c4c56b563a2c01f0668c0351
rajendrag <it.rajendra@gmail.com>
2015-12-17 00:14:03 -0800

    - count words example

--- src/main/scala/com/sip/chapter4/MapEx.scala
+++ src/main/scala/com/sip/chapter4/MapEx.scala
@@ -1,11 +1,15 @@
 package com.sip.chapter4
 
+import scala.io.Source
+
 /**
   * Created by RP on 11/30/15.
   */
 object MapEx {
   def main(args: Array[String]) {
     gizmosDiscount
+    countWordsImperative
+    countWordsDeclerative
   }
 
   /**
@@ -15,6 +19,32 @@ object MapEx {
   def gizmosDiscount(): Unit = {
     val gizmos = Map(("gizmo1", 20.0), ("gizmo2", 30.0), ("gizmo3", 40.0), ("gizmo4", 50.0), ("gizmo5", 60.0), ("gizmo6", 70.0))
     val newGizmos = gizmos map {case (k, v) => (k, v - (v/10))}
-    printf(newGizmos.mkString(","))
+    println(newGizmos.mkString(","))
+  }
+
+  /**
+    * Write a program that reads words from a file. Use a mutable map to count how often each word appears.
+    * To read the words, simply use a java.util.Scanner:
+    * val in = new java.util.Scanner(new java.io.File("myfile.txt"))
+    * while (in.hasNext()) process in.next()
+    * Or look at Chapter 9 for a Scalaesque way.
+    * At the end, print out all words and their counts.
+    */
+  def countWordsImperative() = {
+    val in = new java.util.Scanner(new java.io.File("myfile.txt"))
+    val dictionary = collection.mutable.Map[String, Int]()
+    while (in.hasNext()) {
+      for(x <- in.next().split(" ")) {
+        val count = dictionary.getOrElse(x, 0)
+        dictionary(x) = count + 1
+      }
+    }
+    println(dictionary.mkString(","))
+  }
+
+  def countWordsDeclerative() = {
+    val words = Source.fromFile("myfile.txt").getLines().flatMap(_.split(" "))
+    println(words.mkString(", "))
+    //words.gr
   }
 }
